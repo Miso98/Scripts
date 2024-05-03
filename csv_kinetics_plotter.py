e658b860d2cdb3d4a9e1c6afd1fee67b1daf224e
Mitchell So <110700396+Miso98@users.noreply.github.com>
2024-05-03 10:30:21 -0700

    Update csv_kinetics_plotter.py
    updated to allow for subplots

--- csv_kinetics_plotter.py
+++ csv_kinetics_plotter.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 
@@ -7,30 +6,29 @@ import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 import scipy.optimize as opt
+from scipy.optimize import curve_fit
 matplotlib.rcParams.update({'font.size': 16})
 
 absorbtivity = 20000 * 0.24 
 
-def read_lockin_data(file, delimeter=";", channels=1 ):
-    if channels==1:
-        skiprows=5
-    elif channels==2:
-        skiprows=7
-    df = pd.read_csv(file, delimiter=delimeter, skiprows=skiprows).to_numpy()
+#reading CSV for raw lockin voltage data 
+def read_lockin_data(file, delimiter=";", channels=1):
+    if channels == 1:
+        skiprows = 5
+    elif channels == 2:
+        skiprows = 7
+    df = pd.read_csv(file, delimiter=delimiter, skiprows=skiprows).to_numpy()
     if channels > 1:
         channel2_index = np.where(abs(df[1:,0]-df[:-1,0]) > 0.5*np.max(abs(df[:,0])))[0]
-        # if len(channel2_index) > 1:
-        #     channel2 = df[channel2_index[0]+1:,1]
-        #     df = np.concatenate((df[:channel2_index[0]+1,:], channel2.reshape(len(channel2),1)), axis=1)
-        # if len(df[:,0]) % 2 == 1:
-        #     df = df[:-1,:]
         channel2 = df[channel2_index[0]+1:,1]
         if len(channel2) != len(df[:channel2_index[0]+1,0]):
-            channel2 = channel2[:min( len(channel2), len(df[:channel2_index[0]+1,0]) )]
-            df = df[:min( len(channel2), len(df[:channel2_index[0]+1,0]) ), :]
+            channel2 = channel2[:min(len(channel2), len(df[:channel2_index[0]+1,0]))]
+            df = df[:min(len(channel2), len(df[:channel2_index[0]+1,0])), :]
         df = np.concatenate((df[:channel2_index[0]+1,:], channel2.reshape(len(channel2),1)), axis=1)
     return df
 
+
+#converting lockin voltage data into a transmission value
 def vol2transmission(df, zero_val=None):
     df2 = df.copy()
     if zero_val is None:
@@ -42,6 +40,8 @@ def vol2transmission(df, zero_val=None):
         df2[:,1] = df2[:,1] * zero_val**-1
     return df2
 
+
+#adjusting the weird lock in time values that are read in milliseconds
 def adjust_time(df,cutoff=None, ms=False, minutes=True):
     if cutoff is not None:
         df = df[df[:,1]<cutoff,:]
@@ -55,139 +55,77 @@ def adjust_time(df,cutoff=None, ms=False, minutes=True):
 def plot(df, label=""):
     plt.plot(df[:,0], df[:,1:], label=label)
 
-#possible reaction rates (returns transmission)
-def logistic_rxn(t, Cmax, k, t0):
-    C = Cmax * (1 + np.exp(-1*k*(t - t0)))**-1
-    T = 10**(-1*absorbtivity*C)
-    return T
-def exponential_rxn(t, Cmax, k, t0):
-    C = Cmax * (1 - np.exp(-1*k*(t-t0)))
-    T = 10**(-1*absorbtivity*C)
-    return T
-def secondorder_rxn(t, Cmax, k, alpha, t0):
-    C = Cmax*(1 - (-1*(t-t0)*k*(1 - alpha) + 1)**(1/(1-alpha)))
-    T = 10**(-1*absorbtivity*C)
-    return T
-def double_exp_rxn(t, Cmax, k1, k2, t0):
-    C = Cmax * (1 - 1*(k2*np.exp(-1*k1*(t-t0)) -k1*np.exp(-1*k2*(t-t0))) / (k2-k1))
-    T = 10**(-1*absorbtivity*C)
-    return T
+#not currently fitting the absorbtivity factor
+  
+def exp_func(x, a, b):
+        return a*  np.exp(b * x)
 
-def triple_exp_rxn(t, Cmax, k1, k2, k3, t0):
-    #not currently fitting the absorbtivity factor
-    agg_factor = 10.0
-    
-    C3 = Cmax*k1*k2*( np.exp(-1*k1*(t-t0)) / ( (k1-k2)*(k1-k3) )
-                     + np.exp(-1*k2*(t-t0)) / ( (k2-k1)*(k2-k3) )
-                     + np.exp(-1*k3*(t-t0)) / ( (k3-k1)*(k3-k2) )
-                     )
-    C4 = Cmax + Cmax / ( (k1-k2)*(k2-k3)*(k1-k3) ) * (
-        k2*k3*(k3-k2)*np.exp(-1*k1*(t-t0))
-        + k1*k3*(k1-k3)*np.exp(-1*k2*(t-t0))
-        + k1*k2*(k2-k1)*np.exp(-1*k3*(t-t0)))
-    
-    T = 10**( -1*absorbtivity*(C3 + agg_factor*C4) )
-    return T
-
-#folder = "C:/Users/jmyles/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/"
-#1 day kinetics
-#folder = "C:/Users/mso/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/Kinetics data 4-18-24 asc stability 100uM/1 Day/"
-#7 day kinetics
-#folder = "C:/Users/mso/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/Kinetics data 4-18-24 asc stability 100uM/7 Day/"
-#repeatability experiment
-#folder = "C:/Users/mso/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/Kinetics data 4-23-24 repeatability 94uM/"
-folder = "C:/Users/mso/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/20240424 Ladder/"
-#cutoff = 0.75
-#cutoff is the value to start plotting at, so for cases of 0uM and 1uM the df will be a null set unless specified to a higher cutoff of transmission here it is 10
-cutoff = 10
-
-
-files = ["20240424-Ladder-0uM.txt",
-         "20240424-Ladder-1uM.txt",
-         "20240424-Ladder-5uM.txt",
-         "20240424-Ladder-20uM-after-3min.txt",
-         "20240424-Ladder-150uM.txt",
-         "20240424-Ladder-250uM.txt"
+cutoff = 0.75
+#cutoff is the value to start plotting, so for 0uM and 1uM the df will be a null set 
+#unless specified to a higher cutoff of transmission here it is 10
+#cutoff = 10
+#folder = "C:/Users/Mitch So/OneDrive/Documents/Python files/"
+folder = "C:/Users/mso/Leiden Tech Dropbox/Projects - Shared/2008 - Phosphator Phase II/8-Data/Lock-in Optics Testing Data/Kinetics Data/20240501 Asc 14 day stability/"
+files = ["20240501-14Day-5C-1.txt",
+         "20240501-14Day-5C-2.txt",
+         "20240501-14Day-20C-2.txt",
+         "20240501-14Day-20C-1.txt",
+         "20240501-14Day-35C-1.txt",
+         "20240501-14Day-35C-2.txt"
          ]
-baselines = ["20240424-Ladder-Baseline-1.txt",
-             "20240424-Ladder-Baseline-2.txt",
-             "20240424-Ladder-Baseline-3.txt",
-             "20240424-Ladder-Baseline-4.txt",
-             "20240424-Ladder-Baseline-5.txt",
-             "20240424-Ladder-Baseline-6.txt"
+baselines = ["20240501-Baseline-1.txt",
+             "20240501-Baseline-2.txt",
+             "20240501-Baseline-3.txt",
+             "20240501-Baseline-4.txt",
+             "20240501-Baseline-5.txt",
+             "20240501-Baseline-6.txt"
          ]
 
-labels = ["0uM",
-          "1uM",
-          "5uM",
-          "20uM",
-          "150uM",
-          "250uM"
+labels = ["5C-1",
+          "5C-2",
+          "20C-1",
+          "20C-2",
+          "35C-1",
+          "35C-2"
           ]
-
 pin0=True
-plt.figure(figsize=(12,9))
+#plt.figure(figsize=(12,9))
+exp_temps = [5, 20, 35]
+fig, axs = plt.subplots(1, len(exp_temps), figsize=(15,6))
 
-for file, baseline_path, label in zip(files, baselines, labels):
-    baseline = read_lockin_data(
-        folder + baseline_path,
-        delimeter=";", channels=2)
-    
-    experiment = read_lockin_data(
-        folder + file,
-        delimeter=";", channels=2)
-    experiment = vol2transmission(experiment, zero_val=np.array([np.average(baseline[:,1]), np.average(experiment[:,2])]))
-    experiment = adjust_time(experiment[:,:2], cutoff=cutoff)
-    plt.plot(experiment[:,0], experiment[:,1], label=label)
-    
-    
-    half = int(len(experiment)/1)
-    # half = np.argmax(experiment[:,0] > 5) #3 minutes
-    if pin0:
-        temp = experiment[0,1] * np.ones((int(half/4),2))
-        temp[:,0] = 0
-        experiment =np.concatenate((temp, experiment[:,0:2]))
-        
-        
-    # f = triple_exp_rxn
-    # popt, pcov = opt.curve_fit(f, 
-    #                             experiment[:half,0], experiment[:half,1],
-    #                             bounds=([0, 2e-3,1e-3,0.5e-6,-1e3],[1e-3, 1e1, 1e1, 3e-4, 1e3]),
-    #                             x_scale=[1000, 1, 1, 10, 1]
-    #                             )
-    # print(popt[3])
-    # f = double_exp_rxn
-    # popt, pcov = opt.curve_fit(f, 
-    #                             experiment[:half,0], experiment[:half,1],
-    #                             bounds=([0, 2e-3,1e-3,-1e1],[1e-3, 1e1, 1e1, 1e1]),
-    #                             x_scale=[1000, 1, 1,  1]
-    #                             )
-    
-    
-    #removing the curve fitting for exponential reaction 
-    
-    #f = exponential_rxn
-    #popt, pcov = opt.curve_fit(f, 
-                                #experiment[:half,0], experiment[:half,1],
-                                #bounds=([0, 2e-3,-1e3],[1e-3, 1e1,  1e3]),
-                                #x_scale=[1000, 1,  1]
-                                #)
-    # print(popt)
-    # plt.axhline(10**(-1*popt[0]*absorbtivity), color="grey")
-   # print(str(np.sum((f(experiment[:,0], *popt) - experiment[:,1])**2)/half ) 
-    #      + ", " + str(popt[0]) )
-    
-    # plt.semilogy(experiment[:,0], 1*np.log10(f(experiment[:,0], *popt))/absorbtivity + popt[0], "--",label=label)
-    # plt.semilogy(experiment[:,0], 1*np.log10(experiment[:,1:])/absorbtivity  + popt[0],label=label)
-    #plt.plot(experiment[:,0], experiment[:,1], label=label)
-   # plt.plot(experiment[:,0], (f(experiment[:,0], *popt)), "--", label=label)
-    
+#iterating through each file and fitting a curve to add to subplot   
+for i in range(len(exp_temps)):
 
-plt.xlabel("time (min)")
-plt.ylabel("Transmission")
-plt.ylim([-0.01,1.01])
-plt.title("Reaction Kinetics Ladder")
-plt.legend()
+    baseline = read_lockin_data(folder + baselines[2*i], delimiter=";", channels=2)
+    experiment = read_lockin_data(folder + files[2*i], delimiter=";", channels=2)
+    
+    # Convert voltage to transmission and adjust time
+    experiment = vol2transmission(experiment, zero_val=np.array([np.average(baseline[:, 1]), np.average(experiment[:, 2])]))
+    experiment = adjust_time(experiment[:, :2], cutoff=cutoff)
+    transmission_values = experiment[:,1]   
+    time_values = experiment[:,0]
+    # Scatter plot of original data
+    axs[i].scatter(experiment[:, 0], experiment[:, 1], label='Original data')
+    
+    # Curve fitting
+    popt, pcov = curve_fit(exp_func, experiment[:, 0], experiment[:, 1])
+    x_fit = np.linspace(min(time_values), max(time_values), 100)
+    y_fit = exp_func(x_fit, *popt)
+    axs[i].plot(x_fit, y_fit, color='red', label='Exponential Curve Fit')
+    
+    #  Set labels and title
+    axs[i].set_xlabel('Time (min)')
+    axs[i].set_ylabel('Transmission')
+    axs[i].grid(True)
+    axs[i].legend()
+    axs[i].set_title(f'{exp_temps[i]}°C')
+    
+    # Display the equation of the exponential curve
+    equation_text = f'y = {popt[0]:.2f} * e^({popt[1]:.2f} * x)'
+    axs[i].text(0.1, 0.1, equation_text, transform=axs[i].transAxes, fontsize=10, verticalalignment='top')
+
 plt.tight_layout()
-#does not automatically plot graph needs this for powershell and embedded vs terminal
 plt.show()
+
+
+
